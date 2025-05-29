5d6abd12d25419a3c0c86d690677efd13fcc79aa
Mohd Fazal Ali <fazalalicontribute@gmail.com>
2025-05-29 17:43:04 +0530

    Update axiosInstance.js

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"https://url-shortner-backend-cxaj.onrender.com",
+    baseURL:"http://localhost:3000/",
     timeout:10000, //10s
     withCredentials:true
 })
